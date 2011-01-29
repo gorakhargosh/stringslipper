49c97078db726b6013ee7e24d7f2429a4f0587e2
Gora Khargosh <gora.khargosh@gmail.com>
2011-01-29 10:31:30 +0530

    Bumps version to 0.1
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def read_file(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(name='quickslipper',
-      version='0.0.1',
+      version='0.1',
       description='Quicksilver-like pattern matching',
       long_description=read_file('README'),
       author="Gora Khargosh",
